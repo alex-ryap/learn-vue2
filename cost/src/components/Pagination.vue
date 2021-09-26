89789915ee11297506c34299147fbf52acfcc236
Alexandr <alexryapolov36@gmail.com>
2021-09-26 23:04:14 +0400

    add table and form for costs

---
+++ cost/src/components/Pagination.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="pagination">
+    <button v-for="page in pagesCount" :key="page">
+      {{ page }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: {
+    itemsCount: {
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      pagesCount: null,
+      countItemOfPage: 5,
+    };
+  },
+  methods: {
+    generatePages() {
+      return Math.ceil(this.itemsCount / this.countItemOfPage);
+    },
+  },
+  created() {
+    this.pagesCount = this.generatePages();
+  },
+};
+</script>
+
+<style lang="scss"></style>
